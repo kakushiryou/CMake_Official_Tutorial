b3dc688569fea807fceac54a5edbd84f2af452e0
GUO Siliang <kakushiryou@outlook.com>
2024-01-21 21:55:11 +0800

    Lesson 1 Exercise 3
    1. 学习了 Lesson 1 Exercise 3 剩下的内容；
    2. 学习的道路还很长~

---
+++ build/TutorialConfig.h
@@ -0,0 +1,3 @@
+#define PROJECT_VERSION_MAJOR 1
+#define PROJECT_VERSION_MINOR 3
+#define PROJECT_STR "Hello World from CMakeLists.txt!"
--- tutorial.cxx
+++ tutorial.cxx
@@ -3,30 +3,29 @@
 #include <iostream>
 #include <string>
 
-// TODO 11: Include TutorialConfig.h
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2) {
-	// TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
-	//          and Tutorial_VERSION_MINOR
-	std::cout << "Usage: " << argv[0] << " number" << '\n';
-	return 1;
+	if (argc < 2)
+	{
+		std::cout << "Usage: " << argv[0] << " number" << '\n';
+		return 1;
 	}
 
 	// 在 VS Code 中设置该文件的编码为 UTF-8 with BOM 可以解决中文输出乱码的问题。
 	// MSVC 输出 __cplusplus 总是 199711，需要在 CMake 中额外进行配置。
 	std::cout << "C++ 版本为：" << __cplusplus << '\n';
-	
-	
+	std::cout << "项目大版本号为：" << PROJECT_VERSION_MAJOR << '\n';
+	std::cout << "项目小版本号为：" << PROJECT_VERSION_MINOR << '\n';
+	std::cout << "项目中的字符串：" << PROJECT_STR << '\n';
 
-	// convert input to double
-	// TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
+	// 将输入转换为 double:
+	// Replace atof(argv[1]) with std::stod(argv[1])
 	const double inputValue = std::stod(argv[1]);
 
-	// calculate square root
+	// 计算平方差:
 	const double outputValue = sqrt(inputValue);
-	std::cout << "The square root of " << inputValue << " is " << outputValue
-			<< std::endl;
+	std::cout << "The square root of " << inputValue << " is " << outputValue << '\n';
 	return 0;
 }
