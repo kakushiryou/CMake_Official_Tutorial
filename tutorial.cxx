d2e0fe4872dc79dc68eb593007cfc8b903a86880
GUO Siliang <kakushiryou@outlook.com>
2024-01-21 20:57:03 +0800

    Lesson 1 Exercise 2
    1. 学习了 Lesson 1 Exercise 2 TODO 4~6 的内容；
    2. 将 VS Code 自动生成的 .vscode/settings.json 文件放进了 .gitignore 中；
    3. 在 build 文件夹下创建了一个空的 .gitkeep 文件，忽略了其他文件的上传。

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,6 +1,5 @@
-// A simple program that computes the square root of a number
+﻿// A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
 #include <iostream>
 #include <string>
 
@@ -8,20 +7,26 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2) {
-    // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
-    //          and Tutorial_VERSION_MINOR
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+	if (argc < 2) {
+	// TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
+	//          and Tutorial_VERSION_MINOR
+	std::cout << "Usage: " << argv[0] << " number" << '\n';
+	return 1;
+	}
 
-  // convert input to double
-  // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  const double inputValue = atof(argv[1]);
+	// 在 VS Code 中设置该文件的编码为 UTF-8 with BOM 可以解决中文输出乱码的问题。
+	// MSVC 输出 __cplusplus 总是 199711，需要在 CMake 中额外进行配置。
+	std::cout << "C++ 版本为：" << __cplusplus << '\n';
+	
+	
 
-  // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-  return 0;
+	// convert input to double
+	// TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
+	const double inputValue = std::stod(argv[1]);
+
+	// calculate square root
+	const double outputValue = sqrt(inputValue);
+	std::cout << "The square root of " << inputValue << " is " << outputValue
+			<< std::endl;
+	return 0;
 }
